3a907aaaa00a5ad6989285519cab40d855b1448d
Sam Hatley <sam+github@samhatley.com>
2023-02-13 09:05:20 +0000

    Comments

--- 3_data_warehouse/load_to_bucket.py
+++ 3_data_warehouse/load_to_bucket.py
@@ -17,13 +17,15 @@ def extract_from_ghub(file: str) -> Path:
 
 @task(log_prints=True, retries=3)
 def upload_data(local_path: str, file: str) -> None:
+    """Uploads local data to GCP"""
+
     gcs_block = GcsBucket.load("dezoomcamp1")
     gcs_block.upload_from_path(from_path=local_path, to_path=f"fhv/{file}")
     return
 
 
-@flow
-def flow():
+@flow(log_prints=True)
+def main() -> None:
     years = [2019, 2020, 2021]
     for year in years:
         for month in range(1, 13):
@@ -31,8 +33,8 @@ def flow():
                 break
             file = f"fhv_tripdata_{year}-{month:02}.csv.gz"
             local_path = extract_from_ghub(file)
-            # upload_data(local_path, file)
+            upload_data(local_path, file)
 
 
 if __name__ == "__main__":
-    flow()
+    main()

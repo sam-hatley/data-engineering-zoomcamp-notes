c94afe1a36d740f6028ba39b4996d2f047670f23
sam-hatley <sam+github@samhatley.com>
2023-03-18 16:20:00 +0000

    Updated functional streaming

--- 6_stream_processing/homework/streaming.py
+++ 6_stream_processing/homework/streaming.py
@@ -50,50 +50,9 @@ def sink_console(df, output_mode: str = "complete", processing_time: str = "5 se
     return write_query  # pyspark.sql.streaming.StreamingQuery
 
 
-def sink_memory(df, query_name, query_template):
-    query_df = df.writeStream.queryName(query_name).format("memory").start()
-    query_str = query_template.format(table_name=query_name)
-    query_results = spark.sql(query_str)
-    return query_results, query_df
-
-
-def sink_kafka(df, topic):
-    write_query = (
-        df.writeStream.format("kafka")
-        .option("kafka.bootstrap.servers", "localhost:9092,broker:29092")
-        .outputMode("complete")
-        .option("topic", topic)
-        .option("checkpointLocation", "checkpoint")
-        .start()
-    )
-    return write_query
-
-
-def prepare_df_to_kafka_sink(df, value_columns, key_column=None):
-    columns = df.columns
-
-    df = df.withColumn("value", F.concat_ws(", ", *value_columns))
-    if key_column:
-        df = df.withColumnRenamed(key_column, "key")
-        df = df.withColumn("key", df.key.cast("string"))
-    return df.select(["key", "value"])
-
-
 def op_groupby(df, column_names):
-    df_aggregation = df.groupBy(column_names).count()
+    df_aggregation = df.groupBy(column_names).count().sort(F.desc("count"))
     return df_aggregation
-
-
-def op_windowed_groupby(df, window_duration, slide_duration):
-    df_windowed_aggregation = df.groupBy(
-        F.window(
-            timeColumn=df.pickup_datetime,
-            windowDuration=window_duration,
-            slideDuration=slide_duration,
-        ),
-        df.id,
-    ).count()
-    return df_windowed_aggregation
 
 
 if __name__ == "__main__":
@@ -110,21 +69,9 @@ if __name__ == "__main__":
 
     sink_console(df_rides, output_mode="append")
 
-    df_trip_count_by_id = op_groupby(df_rides, ["id"])
-    df_trip_count_by_pickup_date_id = op_windowed_groupby(
-        df_rides, window_duration="10 minutes", slide_duration="5 minutes"
-    )
+    df_trip_count_by_id = op_groupby(df_rides, ["pulocationid"])
 
     # write the output out to the console for debugging / testing
     sink_console(df_trip_count_by_id)
-    # write the output to the kafka topic
-    df_trip_count_messages = prepare_df_to_kafka_sink(
-        df=df_trip_count_by_pickup_date_id,
-        value_columns=["count"],
-        key_column="id",
-    )
-    kafka_sink_query = sink_kafka(
-        df=df_trip_count_messages, topic="vendor_counts_windowed"
-    )
 
     spark.streams.awaitAnyTermination()
